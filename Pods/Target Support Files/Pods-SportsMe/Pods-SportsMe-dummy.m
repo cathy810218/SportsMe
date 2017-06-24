726047cee949efb1208606d69d20faf8925d65cc
cathy810218 <cathy810218@gmail.com>
2017-06-24 11:44:18 -0700

    Add cell nib and add SDWebImage pod

---
+++ Pods/Target Support Files/Pods-SportsMe/Pods-SportsMe-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SportsMe : NSObject
+@end
+@implementation PodsDummy_Pods_SportsMe
+@end
---
+++ SportsMe/GameMatchCell.m
@@ -0,0 +1,24 @@
+//
+//  GameMatchCell.m
+//  SportsMe
+//
+//  Created by Cathy Oun on 6/24/17.
+//  Copyright © 2017 Cathy Oun. All rights reserved.
+//
+
+#import "GameMatchCell.h"
+
+@implementation GameMatchCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
