31457b87153d0f42e0d108e366d0419461d3d6fc
yanil3500 <yanil3500@gmail.com>
2017-06-24 17:26:05 -0700

    working on mlb json parser

--- SportsMe/BaseballAPI.m
+++ SportsMe/BaseballAPI.m
@@ -8,43 +8,30 @@
 
 #import "BaseballAPI.h"
 #import "APIKeys.h"
-@interface BaseballAPI() <NSXMLParserDelegate>
-@property(strong, nonatomic) NSXMLParser *xmlParser;
+@interface BaseballAPI()
 @property(strong, nonatomic) NSString *textNode;
 @property(strong, nonatomic) NSString *elementName;
+@property(strong, nonatomic) NSDateFormatter *dateFormatter;
 @end
 
 @implementation BaseballAPI
 
+-(NSString *)getTodaysDate{
+    self.dateFormatter = [[NSDateFormatter alloc]init];
+    [self.dateFormatter setDateFormat:@"yyyy/MM/dd"];
+    return [self.dateFormatter stringFromDate:[NSDate date]];
+}
+
 -(void)beginParsing{
-    NSString *urlString = [[NSString alloc] initWithFormat:@"https://api.sportradar.us/mlb-t5/games/2017/06/24/schedule.xml?api_key=%@", secretKey];
-    NSURL *url = [[NSURL alloc]initWithString:urlString];
-    self.textNode = [[NSString alloc] init];
-    self.elementName = [[NSString alloc]init];
-    NSXMLParser * parser = [[NSXMLParser alloc] initWithContentsOfURL:url];
-    self.xmlParser = parser;
-    self.xmlParser.delegate = self;
-    [self.xmlParser parse];
+    NSString *todaysDate = [self getTodaysDate];
+    NSError *error;
+    NSString *urlString = [[NSString alloc] initWithFormat:@"https://api.sportradar.us/mlb-t5/games/%@/schedule.json?api_key=%@",todaysDate, secretKey];
+    NSURL *url = [[NSURL alloc] initWithString:urlString];
+    NSData *data = [NSData dataWithContentsOfURL:url];
+    NSMutableArray *json =[NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:&error];
     
-    
-}
--(void)parser:(NSXMLParser *)parser didStartElement:(NSString *)elementName namespaceURI:(NSString *)namespaceURI qualifiedName:(NSString *)qName attributes:(NSDictionary<NSString *,NSString *> *)attributeDict{
-    self.elementName = elementName;
-    if([elementName  isEqual: @"game"]){
-        NSLog(@"Statuses: %@", attributeDict[@"status"]);
-    }
-}
-
--(void)parser:(NSXMLParser *)parser didEndElement:(NSString *)elementName namespaceURI:(NSString *)namespaceURI qualifiedName:(NSString *)qName{
     
 }
 
--(void)parser:(NSXMLParser *)parser foundCharacters:(NSString *)string{
-    
-}
-
--(void)parser:(NSXMLParser *)parser foundCDATA:(NSData *)CDATABlock{
-    
-}
 
 @end
