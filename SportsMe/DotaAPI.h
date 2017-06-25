4aaf9cf544a1999e4b4db9fffef805ef1fe9810e
Brandon Little <brlittle86@gmail.com>
2017-06-24 19:30:37 -0700

    added api call functionality for Dota 2

---
+++ SportsMe/DotaAPI.h
@@ -0,0 +1,15 @@
+//
+//  DotaAPI.h
+//  SportsMe
+//
+//  Created by Brandon Little on 6/24/17.
+//  Copyright © 2017 Cathy Oun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DotaAPI : NSObject
+
+-(void)fetchDotaDataWithCompletion:(void (^)(NSArray *games))completion;
+
+@end
